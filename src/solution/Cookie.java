4c1decf685b5093fd8e4acb4d15f2fd2f3fa7c4f
Lim_Yu_Aun <limyuaun3@gmail.com>
2024-10-17 16:12:12 +0800

    commit

--- src/solution/Cookie.java
+++ src/solution/Cookie.java
@@ -1,44 +1,111 @@
-package solution;
-
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Cookie {
-    List<String> cookies  = new ArrayList<>();
+public class Cookie implements Runnable {
 
-    public void readCookie(String dirPathFileName) throws FileNotFoundException, IOException{
+    List<String> cookies = new ArrayList<String>();
+
+    Socket s;
+    String fullPathName;
+
+    public Cookie(Socket s, String dirPathFileName) {
+        this.s = s;
+        this.fullPathName = dirPathFileName;
+    }
+
+    public void readCookieFile(String dirPathFileName) throws IOException {
+
         FileReader fr = new FileReader(dirPathFileName);
         BufferedReader br = new BufferedReader(fr);
 
-        String cookie = ""; 
+        String cookie = "";
         while ((cookie = br.readLine()) != null) {
-            cookies.add(cookie);            
+            cookies.add(cookie);
         }
         br.close();
         fr.close();
     }
 
-    public void printCookies(){
-        if(cookies.size() > 0){
-            cookies.forEach(System.out:: println);
+    public String getRandomCookie() {
+        Random rand = new Random();
+
+        if (cookies != null) {
+            if (cookies.size() > 0) {
+                return cookies.get(rand.nextInt(cookies.size()));
+
+            } else {
+                return "No cookie found!!!";
+            }
+        }
+
+        return "No cookie found!!!";
+    }
+
+    public void printCookies() {
+        if (cookies.size() > 0) {
+            cookies.forEach(System.out::println);
         }
     }
 
-    public String getCookie(){
-        Random rand = new Random();
-        if(cookies != null){
-            if(cookies.size() >0){
-                return cookies.get(rand.nextInt(cookies.size()));
-            }else{
-                return "No cookies found";
+    @Override
+    public void run() {
+        // day 04 - slide 9, 10
+        try {
+            readCookieFile(this.fullPathName);
+            
+            InputStream is;
+            try {
+                is = s.getInputStream();
+                BufferedInputStream bis = new BufferedInputStream(is);
+                DataInputStream dis = new DataInputStream(bis);
+                String messageReceived = "";
+
+                OutputStream os = s.getOutputStream();
+                BufferedOutputStream bos = new BufferedOutputStream(os);
+                DataOutputStream dos = new DataOutputStream(bos);
+
+                // day 04 - slide 9
+                while (!messageReceived.toLowerCase().equals("quit")) {
+                    System.out.println("Waiting for client input...");
+
+                    messageReceived = dis.readUTF(dis);
+
+                    // pick a random cookie
+                    String retrievedCookie = getRandomCookie();
+
+                    // put it to the DataOutputStream to send back to client
+                    dos.writeUTF(retrievedCookie);
+                    dos.flush();
+                }
+
+                dos.close();
+                bos.close();
+                os.close();
+
+                dis.close();
+                bis.close();
+                is.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
+        } catch (Exception ex) {
+            System.err.println(ex.toString());
+        } finally {
+
         }
-        return "No cookies found";
     }
 
 }
--- src/solution/ServerMain.java
+++ src/solution/ServerMain.java
@@ -1,55 +1,50 @@
-package solution;
-
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 
 public class ServerMain {
-    public static void main(String[] args) {
-        try{
-            String dirPath = " ";
-            int portNum = 0;
-            String fileName = " ";
-            if(args.length > 0){
-                portNum = Integer.parseInt(args[0]);
-                dirPath = args[1];
-                fileName = args[2];
-            }else{
-                System.err.println("Invalid number of arguments expected");
-                System.exit(0);
-            }
-            File newDirectory = new File(dirPath);
-    
-            if (!newDirectory.exists()){
-                System.out.println("New directory created");
-                newDirectory.mkdir();
-            }
-            //read and print cookies
-            Cookie c = new Cookie();
-            c.readCookie(dirPath + File.separator +  fileName);
-            c.printCookies();
 
+    // args[0] ==> port number
+    // args[1] => directory path
+    public static void main(String args[]) throws NumberFormatException, IOException {
 
-            ServerSocket server = new ServerSocket(portNum);
-            //Create thread pool - to store the workers
-            ExecutorService thrPool = Executors.newFixedThreadPool(2);
-            System.out.printf("Websocket server started on port: %s\n",portNum );
-
-
-            while(true){
-                Socket sock = server.accept();
-                ClientHandler handler = new ClientHandler(sock,c);
-                thrPool.submit(handler);
-            }
-
-        }
-        catch(IOException ex){
-            ex.printStackTrace();
+        String portNumber = "";
+        String dirPath = "";
+        String fileName = "";
+        if (args.length > 0) {
+            portNumber = args[0];
+            dirPath = args[1];
+            fileName = args[2];
+        } else {
+            System.err.println("Invalid number of arguments expected");
+            System.exit(0);
         }
 
+        File newDirectory = new File(dirPath);
+        if (!newDirectory.exists()) {
+            newDirectory.mkdir();
+        }
+
+        // day 04 - slide 8
+        ServerSocket ss = new ServerSocket(Integer.parseInt(portNumber));
+        Socket s = ss.accept();
+        System.out.printf("\r\nWebsocket server started on port... %s\r\n", portNumber);
+
+        // read and print cookies
+        // Cookie c = new Cookie(s);
+        // c.readCookieFile(dirPath + File.separator + fileName);
+        // c.printCookies();
+
+        Thread th = new Thread(new Cookie(s, dirPath + File.separator + fileName));
+        th.start();
 
 
     }
